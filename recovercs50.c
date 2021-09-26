0b5dadbc940dc78197a6b766b09599e55062261b
Melvin Narayan <80097858+Melvin-n@users.noreply.github.com>
2021-09-27 01:27:34 +1300

    Update recovercs50.c

--- recovercs50.c
+++ recovercs50.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
 
+
+typedef uint8_t BYTE;
 
 int main(int argc, char *argv[])
 {
@@ -8,34 +12,62 @@ int main(int argc, char *argv[])
     if (argc != 2)
     {
         printf("Usage: ./recover image");
+        return 1;
     }
-
+    //OPEN MEM CARD
     //open 2nd parameter file to *f
     FILE *f = fopen(argv[1], "r");
 
     //if unable to open file, return 0
     if(!f)
     {
-        return 0;
+        return 1;
     }
-    //buffer array to hold copied jpg data, counter to keep track of number of jpgs for naming
-    int buffer[512];
-    int counter = 0;
-    //read file f into buffer
-    fread(buffer, sizeof(buffer), 1, f);
 
-    //check if first 4 header numbers match a jpg
-    if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
+    BYTE buffer[512];  //read file f into buffer of 512
+    int counter = 0;
+    int firstjpeg = 0;
+    char imgfile[8];
+    FILE *currentfile;
+    int foundjpg = 0;
+    //REPEAT UNTIL END OF CARD
+    while(fread(buffer, sizeof(BYTE), 512, f) != 0) //while theres still 512 bytes remaining in card
     {
-        //create the filename
-        char *imgfile = " ";
-        sprintf(imgfile, "%03i.jpg", counter);
-        //add to counter
-        counter++;
-        //open new file to write to it
-        FILE *img = fopen(imgfile, "w");
-        //write what's in the buffer, to the new file
-        fwrite(buffer, 512, 1, img);
-        printf("%s", imgfile);
+        //READ 512 BYTES INTO BUFFER
+        //fread(buffer, sizeof(BYTE), 512, f);
+
+        //IF START OF NEW JPG
+        if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
+        {
+            foundjpg = 1;
+            //IF FIRST JPG
+            if (firstjpeg == 0)
+            {
+                firstjpeg = 1;
+            }
+            //IF NOT FIRST THEN CLOSE FILE, OPEN NEW FILE
+            else
+            {
+                fclose(currentfile);
+            }
+            sprintf(imgfile, "%03i.jpg", counter); //create filename
+            currentfile = fopen(imgfile, "w"); //assign new file
+            counter++;
+            fwrite(buffer, sizeof(BYTE), 512, currentfile);
+        }
+        //ELSE IF ALREADY FOUND JPG
+        else
+        {
+            if(foundjpg == 1)
+            {
+                fwrite(buffer, sizeof(BYTE), 512, currentfile); //write what's in the buffer, to the new file
+            }
+
+        }
+    //CLOSE ANY REMANING FILES
+
     }
+    fclose(currentfile);
+    fclose(f);
+    return 0;
 }
