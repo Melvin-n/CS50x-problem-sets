a982a75fa5a25835c17ca3913265ac11603df818
Melvin Narayan <80097858+Melvin-n@users.noreply.github.com>
2021-10-03 14:35:58 +1300

    Update dictionary.c

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -32,20 +32,26 @@ bool check(const char *word)
 {
     // TODO
     int hashval = hash(word);
-    while (table[hashval] != NULL)
+    node *cursor = table[hashval];
+    while (cursor != NULL)
     {
-        if (strcasecmp(table[hashval]->word, word) == 0)
+        if (strcasecmp(cursor->word, word) == 0)
         {
             return true;
         }
         else
         {
-            table[hashval] = table[hashval]->next;
+            cursor = cursor->next;
+
+            if (cursor == NULL)
+            {
+                return false;
+            }
 
         }
     }
+    return 0;
 
-    return false;
 
 }
 
@@ -78,45 +84,56 @@ bool load(const char *dictionary)
 {
     // TODO
     //OPEN DICTIONARY FILE
-    char* currentword[LENGTH + 1];
+    char currentword[LENGTH + 1];
     FILE *dict = fopen(dictionary, "r");
     //check for error in reading file
     if (dict == NULL)
     {
         return false;
     }
-    else
+
+    for (int i = 0; i < N; i++)
     {
-        //declare list of size dictionary to read dict into array list
-        //char *list [sizeof(dict)];
-        //read each word in file into list until returns value is EOF
-
-        while (fscanf(dict, "%c", *currentword) != EOF)
-        {
-            //create memory for a node
-            node *head = NULL;
-            node *n = malloc(sizeof(node));
-            if (n == NULL)
-            {
-                return false;
-            }
-
-            else
-            {
-                //copy word from list into hash table
-                strcpy(n->word, *currentword);
-                //get hash value
-                int hashval = hash(*currentword);
-                //set next val of new node to prev first node
-                n->next = head;
-                //set next val of head to new node
-                head = n;
-                counter ++;
-            }
-        }
+        table[i] = NULL;
     }
+
+    //declare list of size dictionary to read dict into array list
+    //char *list [sizeof(dict)];
+    //read each word in file into list until returns value is EOF
+    while (fscanf(dict, "%s\n", currentword) != EOF)
+    {
+        //create memory for a node
+        node *n = malloc(sizeof(node));
+        if (n == NULL)
+        {
+            return false;
+        }
+
+
+        //copy word from list into hash table
+        strcpy(n->word, currentword);
+        //get hash value
+        int hashval = hash(currentword);
+        //set next val of new node to prev first node
+        if (table[hashval] == NULL)
+        {
+            //if bucket is empty, set the head of the bucket to the node
+            n->next = NULL;
+            table[hashval] = n;
+            counter++;
+        }
+        else
+        {
+            //if not empty, link the current node to the head, then head becomes current node
+            n->next = table[hashval];
+            table[hashval] = n;
+            counter ++;
+        }
+
+    }
+
     fclose(dict);
-    return 0;
+    return true;
 }
 
 // Returns number of words in dictionary if loaded, else 0 if not yet loaded
@@ -129,6 +146,16 @@ unsigned int size(void)
 // Unloads dictionary from memory, returning true if successful, else false
 bool unload(void)
 {
-    // TODO
-    return false;
+    for (int i = 0; i < N; i++)
+    {
+        node *cursor = table[i];
+        while (cursor != NULL)
+        {
+            node *temp = cursor;
+            cursor = cursor->next;
+            free(temp);
+            
+        }
+    }
+    return true;
 }
